323084474bf877027e88885b4b41c3cf60de8cf3
Ian Hornik <yiliansource@gmail.com>
2021-10-08 16:59:18 +0200

    closes #6, fixes site config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,106 +1,97 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'FastJ',
-  tagline: 'The open-source, 2D Java game engine.',
-  url: 'https://tech.fastj',
-  baseUrl: '/',
+  title: "FastJ",
+  tagline: "The open-source, 2D Java game engine.",
+  url: "https://tech.fastj",
+  baseUrl: "/",
   noIndex: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/fastj/fastj_icon.png',
-  organizationName: 'fastjengine', // Usually your GitHub org/user name.
-  projectName: 'fastjengine.github.io', // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/fastj/fastj_icon.png",
+  organizationName: "fastjengine",
+  projectName: "fastjengine.github.io",
   themeConfig: {
     prism: {
-      additionalLanguages: ['java', 'groovy', 'kotlin'],
+      additionalLanguages: ["java", "groovy", "kotlin"],
     },
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
       disableSwitch: false,
       respectPrefersColorScheme: false,
       switchConfig: {
-        darkIcon: '🌙',
-        lightIcon: '❄️'
-      }
+        darkIcon: "🌙",
+        lightIcon: "❄️",
+      },
     },
     navbar: {
       logo: {
-        alt: 'FastJ Logo',
-        src: 'svg/fastj_icon.svg',
+        alt: "FastJ Logo",
+        src: "svg/fastj_icon.svg",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Wiki',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Wiki",
         },
         {
-          href: 'https://javadoc.io/doc/io.github.lucasstarsz.fastj/fastj-library',
-          label: 'API Docs',
-          position: 'left',
-        },
-        { 
-          to: '/news', 
-          label: 'News', 
-          position: 'left' 
+          href: "https://javadoc.io/doc/io.github.lucasstarsz.fastj/fastj-library",
+          label: "API Docs",
+          position: "left",
         },
         {
-          href: 'https://github.com/fastjengine/FastJ/',
-          label: 'GitHub',
-          position: 'right',
+          to: "/news",
+          label: "News",
+          position: "left",
+        },
+        {
+          href: "https://github.com/fastjengine/FastJ/",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Wiki',
-              to: '/wiki/intro',
+              label: "Wiki",
+              to: "/wiki/intro",
             },
             {
-              label: 'API Docs',
-              href: 'https://api.fastj.dev',
+              label: "API Docs",
+              href: "https://api.fastj.dev",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
-            // {
-            //   label: 'Stack Overflow',
-            //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            // },
             {
-              label: 'FastJ Discord',
-              href: 'https://discord.gg/FTWsYVSDv6',
+              label: "FastJ Discord",
+              href: "https://discord.gg/FTWsYVSDv6",
             },
-            // {
-            //   label: 'Twitter',
-            //   href: 'https://twitter.com/docusaurus',
-            // },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'News',
-              to: '/news',
+              label: "News",
+              to: "/news",
             },
             {
-              label: 'GitHub',
-              href: 'https://fastj.dev/',
+              label: "GitHub",
+              href: "https://fastj.dev/",
             },
           ],
         },
       ],
-      copyright: 
-      `Copyright &copy; ${new Date().getFullYear()} Andrew Dey. Built with Docusaurus.
+      copyright: `Copyright &copy; ${new Date().getFullYear()} Andrew Dey. Built with Docusaurus.
       <br/>
       Hand + Feather, Windows, macOS, and Linux icons originally made by 
       <a href="https://www.freepik.com" title="Freepik">Freepik</a> 
@@ -111,33 +102,28 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
-        wiki: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/fastjengine/FastJ/',
-        },
-        news: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/fastjengine/FastJ/',
-        },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         blog: {
-          path: 'news',
-          routeBasePath: 'news',
+          path: "news",
+          routeBasePath: "news",
           postsPerPage: 7,
-          blogDescription: 'Keep up with the latest news on the FastJ Game Engine.',
+          blogTitle: "News",
+          blogDescription:
+            "Keep up with the latest news on the FastJ Game Engine.",
+          editUrl:
+            "https://github.com/fastjengine/fastjengine.github.io/edit/main/",
+          showReadingTime: false,
         },
         docs: {
-          path: 'wiki',
-          routeBasePath: 'wiki',
-        }
+          path: "wiki",
+          routeBasePath: "wiki",
+          editUrl:
+            "https://github.com/fastjengine/fastjengine.github.io/edit/main/",
+        },
       },
     ],
   ],
